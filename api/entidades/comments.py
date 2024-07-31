e051cb3ca442ca39944ad041d81831a203f383c2
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-07-31 17:09:28 -0300

    Entidade comentarios

---
+++ api/entidades/comments.py
@@ -0,0 +1,8 @@
+class Comment():
+    def __init__(self, id, content, post_id, user_id):
+        self.id = id
+        self.content = content
+        self.post_id = post_id
+        self.user_id = user_id
+
+
